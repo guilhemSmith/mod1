b1ff4f4a8f1c71bd67149f128fcb93ca4a13329a
guilhem smith <smith.guilhem@gmail.com>
2021-04-05 14:27:13 +0200

    Renderer struct.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,11 @@
 mod render;
 
-use glutin::dpi::PhysicalSize;
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::{ControlFlow, EventLoop};
-use glutin::window::WindowBuilder;
-use glutin::ContextBuilder;
-
-const SCREEN_WIDTH: u32 = 800;
-const SCREEN_HEIGHT: u32 = 600;
 
 fn main() {
     let events_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title("Glutin Triangle")
-        .with_inner_size(PhysicalSize {
-            width: SCREEN_WIDTH,
-            height: SCREEN_HEIGHT,
-        });
-    let gl_window = ContextBuilder::new()
-        .with_vsync(true)
-        .build_windowed(window, &events_loop)
-        .unwrap();
-
-    let gl_window = unsafe { gl_window.make_current().unwrap() };
-    gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
+    let renderer = render::Renderer::new(&events_loop);
 
     let (shader_program, vao) = render::shader_program::compile("triangle");
 
@@ -34,10 +16,14 @@ fn main() {
             Event::LoopDestroyed => return,
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::CloseRequested => *flow = ControlFlow::Exit,
-                WindowEvent::Resized(size) => render::resize(&gl_window, size),
+                WindowEvent::Resized(size) => renderer.resize(size),
                 _ => (),
             },
-            Event::RedrawRequested(_) => render::clear(&gl_window, shader_program, vao),
+            Event::RedrawRequested(_) => {
+                renderer.clear();
+                renderer.draw(shader_program, vao);
+                renderer.swap();
+            }
             _ => {}
         }
     });
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,18 +1,56 @@
 pub mod shader_program;
 
-use glutin::{dpi::PhysicalSize, window::Window, ContextWrapper, PossiblyCurrent};
+use glutin::{
+	dpi::PhysicalSize,
+	event_loop::EventLoop,
+	window::{Window, WindowBuilder},
+	ContextBuilder, ContextWrapper, PossiblyCurrent,
+};
 
-pub fn clear(gl_window: &ContextWrapper<PossiblyCurrent, Window>, shader_program: u32, vao: u32) {
-	unsafe {
-		gl::ClearColor(0.0, 0.0, 0.0, 1.0);
-		gl::Clear(gl::COLOR_BUFFER_BIT);
-		gl::UseProgram(shader_program);
-		gl::BindVertexArray(vao);
-		gl::DrawArrays(gl::TRIANGLES, 0, 3);
-	}
-	gl_window.swap_buffers().unwrap();
+const SCREEN_WIDTH: u32 = 800;
+const SCREEN_HEIGHT: u32 = 600;
+
+pub struct Renderer {
+	gl_window: ContextWrapper<PossiblyCurrent, Window>,
 }
 
-pub fn resize(gl_window: &ContextWrapper<PossiblyCurrent, Window>, size: PhysicalSize<u32>) {
-	gl_window.resize(size)
+impl Renderer {
+	pub fn new(event_loop: &EventLoop<()>) -> Self {
+		let window = WindowBuilder::new()
+			.with_title("Glutin Triangle")
+			.with_inner_size(PhysicalSize {
+				width: SCREEN_WIDTH,
+				height: SCREEN_HEIGHT,
+			});
+		let gl_window = ContextBuilder::new()
+			.with_vsync(true)
+			.build_windowed(window, &event_loop)
+			.unwrap();
+		let gl_window = unsafe { gl_window.make_current().unwrap() };
+		gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
+
+		Self { gl_window }
+	}
+
+	pub fn clear(&self) {
+		unsafe {
+			gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+			gl::Clear(gl::COLOR_BUFFER_BIT);
+		}
+	}
+
+	pub fn swap(&self) {
+		self.gl_window.swap_buffers().unwrap();
+	}
+	pub fn draw(&self, shader_program: u32, vao: u32) {
+		unsafe {
+			gl::UseProgram(shader_program);
+			gl::BindVertexArray(vao);
+			gl::DrawArrays(gl::TRIANGLES, 0, 3);
+		}
+	}
+
+	pub fn resize(&self, size: PhysicalSize<u32>) {
+		self.gl_window.resize(size)
+	}
 }
